e0251fb278f666af43f0be29956c5f61cd060982
Traczyk <grzegorz.traczyk@capgemini.com>
2025-03-09 09:20:31 +0100

    [codeai] Generate unresolved_build_errors classes

---
+++ src/test/java/bank/BankTransferImplTests.java
@@ -0,0 +1,10 @@
+package org.junit.jupiter.api;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BankTransferImplTest {
+    @Test
+    void testTransfer() {
+        // TODO: Implement your test logic here
+    }
+}
