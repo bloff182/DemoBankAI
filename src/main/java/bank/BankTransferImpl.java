dd91466764b3b57b52e0d40d03202470e2a9a9c3
Traczyk <grzegorz.traczyk@capgemini.com>
2025-03-24 13:17:09 +0100

    GT003-demo: add fee

--- src/main/java/bank/BankTransferImpl.java
+++ src/main/java/bank/BankTransferImpl.java
@@ -2,13 +2,15 @@ package bank;
 
 public class BankTransferImpl implements BankTransfer{
 
+	private final int fee = 1;
+
 	@Override
 	public void transfer(BankAccount from, BankAccount to, int amount) {
 		
-		if(from.getBalance() < amount)
+		if(from.getBalance() < amount + fee)
 			throw new IllegalArgumentException("You have no enought money!!!");
 		
-		from.withdraw(amount);
+		from.withdraw(amount + fee);
 		
 		to.deposit(amount);
 	}
