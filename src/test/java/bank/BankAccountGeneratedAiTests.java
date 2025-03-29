1bd8d9a13a2fae34e406940da0155088add5c933
Traczyk <grzegorz.traczyk@capgemini.com>
2025-03-29 16:33:09 +0100

    GT007-demo: add BankAccountGeneratedAiTests

---
+++ src/test/java/bank/BankAccountGeneratedAiTests.java
@@ -0,0 +1,13 @@
+package bank;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class BankAccountGeneratedAiTests {
+
+    private BankAccount account;
+
+    @BeforeEach
+    public void setup() {
+        account = new BankAccount();
+    }
+}
