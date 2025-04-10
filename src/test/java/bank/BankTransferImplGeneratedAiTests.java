da010fc10bbdbeb183d643401cb039cf400a3ee1
Traczyk <grzegorz.traczyk@capgemini.com>
2025-04-10 17:30:36 +0200

    [codeai] Generate unresolved_build_errors classes

---
+++ src/test/java/bank/BankTransferImplGeneratedAiTests.java
@@ -0,0 +1,42 @@
+package bank;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankTransferImplGeneratedAiTests {
+
+    private BankTransferImpl bankTransferImpl;
+    private BankAccount fromAccount;
+    private BankAccount toAccount;
+
+    @BeforeEach
+    void setUp() {
+        bankTransferImpl = new BankTransferImpl();
+        fromAccount = bankTransferImpl.openAccount(100);
+        toAccount = bankTransferImpl.openAccount(0);
+    }
+
+    @Test
+    void transfer_SuccessfulTransfer() {
+        // GIVEN
+        int amount = 50;
+        // WHEN
+        bankTransferImpl.transfer(fromAccount, toAccount, amount);
+        // THEN
+        assertEquals(50, toAccount.getBalance());
+        assertEquals(50, fromAccount.getBalance());
+    }
+
+    @Test
+    void transfer_InsufficientFunds() {
+        // GIVEN
+        int amount = 150;
+        // WHEN
+        assertThrows(IllegalArgumentException.class, () -> bankTransferImpl.transfer(fromAccount, toAccount, amount));
+        // THEN
+        assertEquals(100, fromAccount.getBalance());
+    }
+
+}
